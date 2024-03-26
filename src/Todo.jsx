3e95a4850d3a5f77092555241ea346f3190f7779
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-03-26 22:48:52 +0600

    add

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,29 @@
+
+// conditional rendering
+
+
+// export default function Task({task, isDone}){
+//     if(isDone===true){
+//         return <li> Finished: {task}</li>
+//     }
+//     return <li> Work On: {task}</li>
+
+// }
+
+
+// Conditional Rendering: using ternary operators
+
+// export default function Task({task, isDone}){
+//     return (
+//     <li> {isDone ?  'Finished' : 'Work on'} : {task}</li>)
+
+// }
+
+
+// Conditional Rendering: using && operators
+
 export default function Task({task, isDone}){
-    if(isDone===true){
-        return <li> Finished: {task}</li>
-    }
-    return <li> Work On: {task}</li>
+    return (
+    <li>  {task} {isDone &&  ' : Finished'} </li>)
 
 }
