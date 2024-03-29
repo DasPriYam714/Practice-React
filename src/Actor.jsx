f736a09716cdafd23093671b0f75b300d6f56cd5
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-03-29 18:14:39 +0600

    add

--- src/Actor.jsx
+++ src/Actor.jsx
@@ -1,3 +1,11 @@
+
+const developerStyles={
+    margin: '20px',
+    border: '2px solid blue',
+    borderRadius: '5px'
+  }
 export default function Actor({name}) {
-return <li>Name: {name}</li>
-}
+return ( 
+<div style={developerStyles}><li>Name: {name}</li>
+</div>
+)}
