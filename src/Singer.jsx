045519e2b0c181a6fd16e1643f015f4fa6b27117
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-03-29 18:19:11 +0600

    add border

--- src/Singer.jsx
+++ src/Singer.jsx
@@ -1,9 +1,16 @@
+const developerStyles={
+    margin: '20px',
+    border: '2px solid red',
+    borderRadius: '5px'
+  }
 export default function Singer({singer}){
     return(
+        <div style={developerStyles}>
         <div>
             <h2>ID: {singer.id} </h2>
             <h3>Name: {singer.name} </h3>
             <h3>Age: {singer.age}</h3>
+        </div>
         </div>
     )
 }
