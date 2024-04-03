da77ee462ac5d4082826a0e6fbeae1686fa200a3
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-04-03 14:16:15 +0600

    add dynamic button using event handler

--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({countryInfo}) => {
+    const [visited,setVisited]= useState(false)
+
+    // For single Click events
+
+    // const handleClick = () => {
+    //     setVisited(true);
+    // }
+
+
+
+// for double click events
+    const handleClick = () => {
+        setVisited(!visited);
+    }
+
+
     // console.log(countryInfo)
     const {name,flags,capital,timezones,region,cca3} = countryInfo;
     console.log(countryInfo);
@@ -14,7 +30,19 @@ const Country = ({countryInfo}) => {
             <p>Timezone:{timezones}</p>
             <p>Region:{region}</p>
             <p>CODE: {cca3}</p>
-            <button className='btn'>Visited</button>
+
+            {/* For single Click Events */}
+
+            {/* <button className='btn' onClick={handleClick}>Visited</button>
+            {visited && 'I visited this country'} */}
+
+
+            {/* For  ternary operator events */}
+            {/* <button className='btn' onClick={handleClick}>Visited</button>
+            {visited ? 'I visited this country' : 'I need to visit this country'} */}
+
+<button className='btn' onClick={handleClick}>{visited ? 'Visited': 'Going'}</button>
+            {visited ? 'I visited this country' : 'I need to visit this country'}
             
         </div>
     );
